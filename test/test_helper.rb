efb1e3c413690a64db2c2a92b8dca70d2acf41c9
Alexander Azarov <self@alaz.me>
2022-07-01 10:51:50 +0100

    remove unnecessary code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'minitest'
-
 class Minitest::Test
   def assert_equal_hash(expected, actual)
     assert_equal expected.sort.reverse.to_h, actual.sort.reverse.to_h
